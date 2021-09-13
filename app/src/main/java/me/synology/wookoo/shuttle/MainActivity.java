fcca8c730ffd820997a412880e737d318e60d5d7
wookoo <nanayagoon@daum.net>
2021-09-13 16:16:47 +0900

    마커추가

--- app/src/main/java/me/synology/wookoo/shuttle/MainActivity.java
+++ app/src/main/java/me/synology/wookoo/shuttle/MainActivity.java
@@ -1,16 +1,42 @@
 package me.synology.wookoo.shuttle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this); //항상 콜백으로 돌려야됨됨
+
+
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {//콜백으로 돌려야
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
 
     }
 }
