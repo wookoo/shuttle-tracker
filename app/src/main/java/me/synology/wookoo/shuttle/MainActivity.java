9bd146c9204b5e7ffa621461b78a26a722d78d2f
wookoo <nanayagoon@daum.net>
2021-09-14 09:08:13 +0900

    마커 위치 변경코드 추가

--- app/src/main/java/me/synology/wookoo/shuttle/MainActivity.java
+++ app/src/main/java/me/synology/wookoo/shuttle/MainActivity.java
@@ -14,6 +14,8 @@ import com.naver.maps.map.overlay.Marker;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    Marker marker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +30,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mapFragment.getMapAsync(this); //항상 콜백으로 돌려야됨됨
 
+        marker  = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+       // marker.setMap(naverMap);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+
+                    runOnUiThread(new Runnable() { //ui thread 에서 call 을 해야됨
+                        @Override
+                        public void run() {
+                            marker.setPosition(new LatLng(37.57,126.978));
+                        }
+                    });
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
 
 
     }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {//콜백으로 돌려야
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+
         marker.setMap(naverMap);
+
 
     }
 }
