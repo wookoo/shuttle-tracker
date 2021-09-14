62a59298cb182d65ae457030e714f0226f1df19c
wookoo <nanayagoon@daum.net>
2021-09-14 09:20:47 +0900

    마커 변경 텍스트 박스 및 버튼 추가

--- app/src/main/java/me/synology/wookoo/shuttle/MainActivity.java
+++ app/src/main/java/me/synology/wookoo/shuttle/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapFragment;
@@ -15,6 +19,11 @@ import com.naver.maps.map.overlay.Marker;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     Marker marker;
+
+    Button change_btn;
+
+    EditText lat_text;
+    EditText long_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mapFragment.getMapAsync(this); //항상 콜백으로 돌려야됨됨
 
+        lat_text = findViewById(R.id.latitude_text);
+        long_text = findViewById(R.id.longitude_text);
+        change_btn = findViewById(R.id.chage_marker_btn);
+
         marker  = new Marker();
         marker.setPosition(new LatLng(37.5670135, 126.9783740));
+
+        change_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double lat = Double.parseDouble(String.valueOf(lat_text.getText()));
+                double lon = Double.parseDouble(String.valueOf(long_text.getText()));
+                Log.d("위도 경도 " , lat + " " + lon);
+                marker.setPosition(new LatLng(lat,lon));
+            }
+        });
+
+
        // marker.setMap(naverMap);
 
         new Thread(new Runnable() {
