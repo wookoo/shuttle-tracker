b3cac210cc2bfe65ce94f2bc3463a7b9a55e9df2
wookoo <nanayagoon@daum.net>
2021-09-16 11:21:41 +0900

    GPS 액티비티 생성

---
+++ app/src/main/java/me/synology/wookoo/shuttle/GPSActivity.java
@@ -0,0 +1,14 @@
+package me.synology.wookoo.shuttle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GPSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpsactivity);
+    }
+}
