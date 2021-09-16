9d051c089416914ab485db8f59ba84f33a39d68b
wookoo <nanayagoon@daum.net>
2021-09-16 11:28:38 +0900

    GPS 권한 수정중

--- app/src/main/java/me/synology/wookoo/shuttle/GPSActivity.java
+++ app/src/main/java/me/synology/wookoo/shuttle/GPSActivity.java
@@ -3,12 +3,23 @@ package me.synology.wookoo.shuttle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GPSActivity extends AppCompatActivity {
+
+    private Button button1;
+    private TextView txtResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gpsactivity);
+        setContentView(R.layout.activity_gps);
+
+
+
+
+
     }
 }
