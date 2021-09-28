43882ad898d35c96dca006a8aeda27a3f35be1e9
wookoo <nanayagoon@daum.net>
2021-09-28 16:01:07 +0900

    회원가입 추가중

--- app/src/main/java/me/synology/wookoo/shuttle/RegisterActivity.java
+++ app/src/main/java/me/synology/wookoo/shuttle/RegisterActivity.java
@@ -3,12 +3,29 @@ package me.synology.wookoo.shuttle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText IDText,passWordText,passWordReText,primaryText,nameText,phoneText;
+    Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        IDText = findViewById(R.id.editTextRegisterID);
+        passWordText = findViewById(R.id.editTextRegisterPassword);
+        passWordReText = findViewById(R.id.editTextRegisterPasswordRe);
+        primaryText = findViewById(R.id.editTextRegisterPrimary);
+        nameText = findViewById(R.id.editTextRegisterName);
+        phoneText = findViewById(R.id.editTextRegisterPhone);
+        registerBtn = findViewById(R.id.registerBtn);
+
+        
+
+
+
     }
 }
